741a3ef9c902588da226ca6d0fc9743042f11ccf
Kevin Lint <kevinlint@toolhouse.com>
2015-06-02 15:07:40 -0700

    wip -refactor

--- bin/dev.js
+++ bin/dev.js
@@ -1,78 +1,48 @@
-// Create a Browsersync instance
-var bs = require("browser-sync").create();
-var UglifyJS = require("uglify-js");
-var UglifyCSS = require("uglifycss");
+// imports
+var uglifyCss = require("uglifyCss");
+var uglifyJs = require("uglify-js");
+var browserSync = require("browser-sync").create();
+var frontMatter = require('front-matter');
 var fs = require('fs');
-var fm = require('front-matter');
+var packageJson = require('../package.json');
 var swig = require('swig');
-var async = require('async');
 
 var buildJs = function() {
-    var js = UglifyJS.minify([
-        "./src/scripts/app.js",
-        "./src/scripts/test.js"
-    ], {
-        outSourceMap: "scripts.js.map",
-        mangle: true,
-        compress: {
-            sequences: true,
-            dead_code: true,
-            conditionals: true,
-            booleans: true,
-            unused: true,
-            if_return: true,
-            join_vars: true,
-            drop_console: true
-        }
-    });
-
-    fs.writeFileSync('./build/scripts.js', js.code);
-    fs.writeFileSync('./build/scripts.js.map', js.map);
+    var js = uglifyJs.minify(packageJson.config.js.files, { outSourceMap: "scripts.js.map" });
+    fs.writeFileSync(packageJson.config.js.output, js.code);
+    fs.writeFileSync(packageJson.config.js.output + '.map', js.map);
 };
 
 var buildCss = function() {
-    var css = UglifyCSS.processFiles([
-        "./src/styles/app.css",
-        "./src/styles/test.css"
-    ], {
-        maxLineLen: 500,
-        expandVars: true
-    });
-
-    fs.writeFileSync('./build/styles.css', css);
+    var css = uglifyCss.processFiles(packageJson.config.css.files);
+    fs.writeFileSync(packageJson.config.css.output, css);
 };
 
 var buildHtml = function() {
-
-    ["./src/html/index"].forEach(function(file) {
-        fs.readFile(file + '.yml', 'utf8', function(err, data) {
+    packageJson.config.html.files.forEach(function(file) {
+        fs.readFile(file.yml, 'utf8', function(err, data) {
             if (err) throw err;
-            var content = fm(data);
-            var html = swig.renderFile(file + '.html', {
-                data: content.attributes,
-                body: content.body
-            });
-            fs.writeFileSync(file.replace('./src/html/', './build/') + '.html', html);
+            var yml = frontMatter(data);
+            var html = swig.renderFile(file.html, { data: yml.attributes, body: yml.body });
+            fs.writeFileSync(file.html.replace('./src/html/', './build/'), html);
         });
     });
-
 };
 
 var build = function() {
     buildJs();
     buildCss();
     buildHtml();
-    bs.reload();
+    browserSync.reload();
 };
 
 // watch dev to rebuild files on change
-bs.watch("./src/html/**/*.{html,yml}").on("change", build);
-bs.watch("./src/scripts/**/*.js").on("change", build);
-bs.watch("./src/styles/**/*.css").on("change", build);
+browserSync.watch("./src/html/**/*.{html,yml}").on("change", build);
+browserSync.watch("./src/scripts/**/*.js").on("change", build);
+browserSync.watch("./src/styles/**/*.css").on("change", build);
 
 // Now init the Browsersync server
-bs.init({
-    server: "./build/",
-});
+browserSync.init({ server: "./build/"});
 
+// build
 build();
