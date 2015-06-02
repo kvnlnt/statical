9e485597cda9fb6d36a167e9413229cf2dadd9fe
Kevin Lint <kevinlint@toolhouse.com>
2015-06-02 09:56:41 -0700

    wip - dev script

--- bin/dev.js
+++ bin/dev.js
@@ -1,32 +1,47 @@
 // Create a Browsersync instance
 var bs = require("browser-sync").create();
-var colors = require('colors');
+var UglifyJS = require("uglify-js");
+var fs = require('fs');
 
-var compile = function() {
-
-
-};
 
 var build = function() {
 
+    // javascript
+    var js = UglifyJS.minify([
+        "./src/scripts/app.js",
+        "./src/scripts/test.js"
+    ], {
+        outSourceMap: "./build/scripts.js.map",
+        mangle: true,
+        compress: {
+            sequences: true,
+            dead_code: true,
+            conditionals: true,
+            booleans: true,
+            unused: true,
+            if_return: true,
+            join_vars: true,
+            drop_console: true
+        }
+    });
 
-};
+    // output
+    fs.writeFileSync('./build/scripts.js', js.code);
 
-var rebuild = function() {
-
-    compile();
-    build();
+    // reload browser
     bs.reload();
 
 };
 
 
-// watch
-bs.watch("./src/html/*").on("change", rebuild);
-bs.watch("./src/scripts/*").on("change", rebuild);
-bs.watch("./src/styles/*").on("change", rebuild);
+// // watch
+// bs.watch("./src/html/*").on("change", rebuild);
+// bs.watch("./src/scripts/*").on("change", rebuild);
+// bs.watch("./src/styles/*").on("change", rebuild);
 
-// Now init the Browsersync server
-bs.init({
-    server: "./build"
-});
+// // Now init the Browsersync server
+// bs.init({
+//     server: "./build"
+// });
+
+build();
