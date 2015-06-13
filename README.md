fe2aa70930da515b74f5bfbc7805b14c3f35cd4f
kvnlnt <kvnlnt@yahoo.com>
2015-06-13 10:47:53 -0700

    refactoring

--- README.md
+++ README.md
@@ -9,8 +9,12 @@ Clone this repo and:
 	npm install
 	npm run dev
 
+## Dependencies
+
+* [libsass](https://github.com/sass/libsass)
+
 ## How it works
-Statical works by treating the content anatomy of a website like that of a russian doll. At the top you have a <code>Property</code> which contains a <code>Page</code> which contains a <code>Part</code> which contains a <code>Piece</code>. You decide how your content should be structured. Each type of content is a series of files that mimic the code-behind architecture. For example a header part might have a header.jst, header.yml and a header.scss file. However, the main concept is that each content tier wraps the tier below it and can pass data to them. This nested architecture allows you to compose reusable and configurable components in an intuitive and methodical way. This approach naturally facilitates a separation of concerns and avoids a number of other common pitfals (complex build scripts, unnecessary dependencies, high learning curve).
+Statical works by treating the content anatomy of a website like that of a russian doll. At the top you have a `Property` which contains a `Page` which contains a `Part` which contains a `Piece`. You decide how your content should be structured. Each type of content is a series of files that mimic the code-behind architecture. For example a header part might have a header.jst, header.yml and a header.scss file. However, the main concept is that each content tier wraps the tier below it and can pass data to them. This nested architecture allows you to compose reusable and configurable components in an intuitive and methodical way. This approach naturally facilitates a separation of concerns and avoids a number of other common pitfals (complex build scripts, unnecessary dependencies, high learning curve).
 
 ## Minimal knowledge required
 Front end development is becoming ridiculous - simply too many tools, apis, build systems, plugins, etc. It's a mess. Statical is an attempt to find the balance between a back to basics approach coupled with the raw power of a number of node modules. To use *Statical* you need to understand the following: 
@@ -22,15 +26,40 @@ Front end development is becoming ridiculous - simply too many tools, apis, buil
 ## Structure
 The folder structure looks like this:
 
-- bin // task scripts
-	- dev.js // the dev/build script
-- build // build code
-- src // source code
-	- property // contains global styles and scripts
-	- pages // contains all pages and their styles and scripts
-	- parts // contains all parts with their styles and scripts
-	- pieces // contains all pieces with their styles and scripts
-	- vendor // contains all vendor styles and scripts
+- bin : _task scripts_
+	- dev.js : _the main dev/build script_
+- build : _the build folder!_
+- src : _all source code_
+	- patterns : _reusable code patterns, library code_
+		- html : _html patterns_
+		- scripts : _js patterns_
+		- styles : _css patterns_
+	- property : _core app styles and scripts_
+	- pages : _all pages and their styles and scripts_
+	- parts : _all parts with their styles and scripts_
+	- pieces : _all pieces with their styles and scripts_
+	- vendor : _all vendor styles and scripts_
+
+## Compilation
+The final ./build/script.js file is compiled synchronously. Reading from the package.json file the compilation goes like this:
+
+	1. patterns/scripts
+	1. pieces
+	1. parts
+	1. pages
+	1. property
+
+This means all your library code (like jquery or custom libs) need to go into the patterns folder. And your main application code needs to go into the property folder, running last after all libs, components, etc have been defined. 
+
+Complication for css is similar but moves the property css up after patterns like so:
+
+	1. patterns/styles
+	1. property
+	1. pieces
+	1. parts
+	1. pages
+
+This order allows for a clean cascading heirarchy. First all your library code (like bootstrap, etc) in patterns and all your styles in property is compiled to establish a foundation. Then pieces, parts and pages are styled. This provides a clean hierarchy of overwrites. A `Piece`, while inheriting whatever base styling is applied can further apply it's own styling with it's code behind file. However, when a piece is included into a `Part` or `Page`, those elements can overwrite the `Pieces` providing contextual styling.
 
 ## Scopes
 There are four scopes matching each element type. A custom swig "varControl" has been setup for each. Scopes are accessed inside .jst or .html files via {{@scopename somevariable }}. The four scopes are:
@@ -54,7 +83,7 @@ Clear as mud? Let's try an example. Let's say you have a page called about-us. I
 	{% include "./src/parts/header.html" %}
 	...A bunch of stuff about us...
 
-This page has a code-behind data file (about-us.yml) with a *title* property. The title is not being output here - although it could be with a <code>{{@page title }}</code>. Instead the page is including a part called *header*. Let's take a look at what the *header* part's files look like:
+This page has a code-behind data file (about-us.yml) with a *title* property. The title is not being output here - although it could be with a `{{@page title }}`. Instead the page is including a part called *header*. Let's take a look at what the *header* part's files look like:
 
 ### ./src/parts/header.yml
 
@@ -82,9 +111,9 @@ See how that works?
 
 ### This is what happened:
 
-- the *header* part *compiled* the <code>{{@part leadline}}</code> block with it's config file and left the <code>{{@page title }}</code> block alone.
+- the *header* part *compiled* the `{{@part leadline}}` block with it's config file and left the `{{@page title }}` block alone.
 - the compiled *header* part was then included into the *about-us* page.
-- where the *about-us* page compiled the <code>{{@page title}}</code> blocks with it's config file.
+- where the *about-us* page compiled the `{{@page title}}` blocks with it's config file.
 
 ## Make sense?
 With this structure, it's easy to *compose* a website with parameterized elements. In the example above the header part could be included into any page, outputting whatever title, etc was specified in that page's relative .yml file.
--- bin/dev.js
+++ bin/dev.js
@@ -1,4 +1,4 @@
-// imports
+// modules
 var browserSync = require("browser-sync").create();
 var del = require('del');
 var frontMatter = require('front-matter');
@@ -13,31 +13,68 @@ var swigProperty = new swig.Swig({ varControls: ['{{@property', '}}'], cache:fal
 var uglifyCss = require("uglifyCss");
 var uglifyJs = require("uglify-js");
 
-// property
-var property = packageJson.property;
+// TOC
+// -----------------------
+// SHORTCUTS
+// HELPERS 
+// BUILD SCRIPTS
 
+// SHORTCUTS
+var patterns = packageJson.build.patterns;
+var property = packageJson.build.property;
+var pages = packageJson.build.pages;
+var parts = packageJson.build.parts;
+var pieces = packageJson.build.pieces;
+
+// HELPERS
 var getFileContents = function(file) {
     return fs.existsSync(file) ? fs.readFileSync(file).toString() : '';
 };
 
+var ensureFilePath = function(file, index){
+
+    var path = file.split('/'); path.pop();
+    var len = path.length-1;
+    var directory = path.splice(0, index+1).join('/');
+
+    // if folder not exists, create it
+    var exists = fs.existsSync(directory);
+    if (!exists) {
+        console.log('create directory: ', directory);
+        fs.mkdirSync(directory);
+    }
+
+    // if not last element, increment and recurse
+    if(index < len){
+        index += 1;
+        ensureFilePath(file, index);
+    }
+};
+
+// BUILD SCRIPTS 
 var buildJs = function(){
+    var patternScripts = patterns.scripts.map(function(script){
+        return './src/patterns/scripts/' + script + ".js";
+    });
+
     var propertyScripts = property.scripts.map(function(script){
         return './src/property/' + script + ".js";
     });
 
-    var pageScripts = property.pages.map(function(script){
+    var pageScripts = pages.map(function(script){
         return './src/pages/' + script + ".js";
     });
 
-    var partScripts = property.parts.map(function(script){
+    var partScripts = parts.map(function(script){
         return './src/parts/' + script + ".js";
     });
 
-    var pieceScripts = property.scripts.map(function(script){
+    var pieceScripts = pieces.map(function(script){
         return './src/pieces/' + script + ".js";
     });
 
-    var scripts = propertyScripts
+    var scripts = patternScripts
+        .concat(propertyScripts)
         .concat(pageScripts)
         .concat(partScripts)
         .concat(pieceScripts)
@@ -49,34 +86,29 @@ var buildJs = function(){
     fs.writeFileSync('./build/scripts.js', js.code);
 };
 
-var buildJsVendor = function(){
-    // concat vendor scripts
-    var vendorScripts = property.vendorScripts.map(function(script){
-        return './src/vendor/' + script;
+var buildCss = function(){
+    var patternStyles = patterns.styles.map(function(style){
+        return './src/patterns/styles/' + style + ".scss";
     });
 
-    var js = vendorScripts.length > 0 ? uglifyJs.minify(vendorScripts) : { code: ''};
-    fs.writeFileSync('./build/vendor.js', js.code);
-};
-
-var buildCss = function(){
     var propertyStyles = property.styles.map(function(style){
         return './src/property/' + style + ".scss";
     });
 
-    var pageStyles = property.pages.map(function(style){
+    var pageStyles = pages.map(function(style){
         return './src/pages/' + style + ".scss";
     });
 
-    var partStyles = property.parts.map(function(style){
+    var partStyles = parts.map(function(style){
         return './src/parts/' + style + ".scss";
     });
 
-    var pieceStyles = property.pieces.map(function(style){
+    var pieceStyles = pieces.map(function(style){
         return './src/pieces/' + style + ".scss";
     });
 
-    var styles = propertyStyles
+    var styles = patternStyles
+        .concat(propertyStyles)
         .concat(pageStyles)
         .concat(partStyles)
         .concat(pieceStyles)
@@ -93,21 +125,10 @@ var buildCss = function(){
     fs.writeFileSync('./build/styles.css', css);
 };
 
-var buildCssVendor = function(){
-    // concat vendor styles
-    var vendorStyles = property.vendorStyles.map(function(style){
-        return './src/vendor/' + style;
-    });
-
-    var css = uglifyCss.processFiles(vendorStyles);
-    fs.writeFileSync('./build/vendor.css', css);
-};
-
 var buildHtml = function(items, path, swigFn, callback) {
     try {
 
         items.forEach(function(item) {
-
             var jstFile = getFileContents(path + item + ".jst");
             var ymlFile = getFileContents(path + item + ".yml");
             var htmlFile = path + item + ".html";
@@ -117,44 +138,45 @@ var buildHtml = function(items, path, swigFn, callback) {
             var compiled = swigCompile(data.attributes);
             fs.writeFileSync(htmlFile, compiled);
             console.log('compile:', htmlFile);
-
         });
 
         if(callback) callback();
 
     } catch (e) {
-
         console.log(e);
-
     }
 };
 
 var buildHtmlProperty = function(){
+
     // swig and copy all pages
-    property.pages.forEach(function(page){
+    pages.forEach(function(page){
         var htmlFile = './src/pages/' + page + ".html";
         var targetFile = './build/' + page + ".html";
         var htmlContents = getFileContents(htmlFile);
         var swigCompile = swigProperty.compile(htmlContents);
         var compiled = swigCompile(property);
+        var dir = targetFile.split('/');
+        ensureFilePath(targetFile,1);
         fs.writeFileSync(targetFile, compiled);
+        console.log('build:', targetFile);
     });
 };
 
 var buildHtmlPages = function(callback) {
-    buildHtml(property.pages, './src/pages/', swigPage, buildHtmlProperty);
+    buildHtml(pages, './src/pages/', swigPage, buildHtmlProperty);
 };
 
 var buildHtmlParts = function(callback) {
-    buildHtml(property.parts, './src/parts/', swigPart, buildHtmlPages);
+    buildHtml(parts, './src/parts/', swigPart, buildHtmlPages);
 };
 
 var buildHtmlPieces = function() {
-    buildHtml(property.pieces, './src/pieces/', swigPiece, buildHtmlParts);
+    buildHtml(pieces, './src/pieces/', swigPiece, buildHtmlParts);
 };
 
 var cleanHtml = function() {
-    del(['./src/**/*.html', './build/*.html'], function(err, paths) {
+    del(['./src/**/*.html', './build/**/*.html'], function(err, paths) {
         console.log('Deleted files/folders:\n', paths.join('\n'));
         buildHtmlPieces();
     });
@@ -164,7 +186,6 @@ var cleanJs = function(){
     del(['./build/*.js'], function(err, paths) {
         console.log('Deleted files/folders:\n', paths.join('\n'));
         buildJs();
-        buildJsVendor();
     });
 };
 
@@ -172,27 +193,28 @@ var cleanCss = function(){
     del(['./build/*.css'], function(err, paths) {
         console.log('Deleted files/folders:\n', paths.join('\n'));
         buildCss();
-        buildCssVendor();
     });
 };
 
-// rebuild on start
-cleanHtml();
-cleanJs();
-cleanCss();
+var cleanAll = function(){
+    cleanHtml();
+    cleanJs();
+    cleanCss();
+};
+
+cleanAll();
 
 // watch dev to rebuild files on change
-browserSync.watch("./src/property/*.{jst,yml}").on("change", buildHtmlProperty);
-browserSync.watch("./src/pages/*.{jst,yml}").on("change", buildHtmlPages);
-browserSync.watch("./src/parts/*.{jst,yml}").on("change", buildHtmlParts);
-browserSync.watch("./src/pieces/*.{jst,yml}").on("change", buildHtmlPieces);
-browserSync.watch("./src/+(property|pages|parts|pieces)/*.js").on("change", buildJs);
-browserSync.watch("./src/vendor/**/*.js").on("change", buildJsVendor);
-browserSync.watch("./src/+(property|pages|parts|pieces)/*.{scss,css}").on("change", buildCss);
-browserSync.watch("./src/vendor/**/*.{scss,css}").on("change", buildCssVendor);
+browserSync.watch("./src/property/**/*.{jst,yml,layout}").on("change", buildHtmlProperty);
+browserSync.watch("./src/pages/**/*.{jst,yml}").on("change", buildHtmlPages);
+browserSync.watch("./src/parts/**/*.{jst,yml}").on("change", buildHtmlParts);
+browserSync.watch("./src/pieces/**/*.{jst,yml}").on("change", buildHtmlPieces);
+browserSync.watch("./src/+(property|pages|parts|pieces)/**/*.js").on("change", buildJs);
+browserSync.watch("./src/+(property|pages|parts|pieces)/**/*.{scss,css}").on("change", buildCss);
+browserSync.watch("package.json").on("change", cleanAll);
 
-// // watch build folder to reload on changes
+// // // watch build folder to reload on changes
 browserSync.watch("./build/**/*").on("change", browserSync.reload);
 
-// // Now init the Browsersync server
+// // // Now init the Browsersync server
 browserSync.init({ server: "./build/"});
