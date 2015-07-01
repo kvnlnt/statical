2d0561cedc198e06d6cc7b39967fb7449a6cec66
Kevin Lint <kevinlint@toolhouse.com>
2015-07-01 14:42:58 -0700

    refactor html swig

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,11 @@
-var gulp = require('gulp');
 var babel = require('gulp-babel');
-var sourcemaps = require('gulp-sourcemaps');
 var concat = require('gulp-concat');
+var gulp = require('gulp');
 var postcss = require('gulp-postcss');
+var sourcemaps = require('gulp-sourcemaps');
+var swig = require('gulp-swig');
+var addsrc = require('gulp-add-src');
+var rimraf = require('gulp-rimraf');
 
 gulp.task('js', function () {
     return gulp.src([
@@ -32,4 +35,34 @@ gulp.task('css', function () {
     .pipe(concat('styles.css'))
     .pipe(sourcemaps.write('.'))
     .pipe(gulp.dest('./build'));
+});
+
+gulp.task('html-pieces', function(){
+    var options = { cache:false, load_json:true, varControls: ['{{@piece', '}}'] };
+    return gulp.src(['./src/pieces/**/*.jst'])
+    .pipe(swig(options))
+    .pipe(gulp.dest('./src/pieces'));
+});
+
+gulp.task('html-parts', function(){
+    var options = { cache:false, load_json:true, varControls: ['{{@part', '}}'] };
+    return gulp.src(['./src/parts/**/*.jst'])
+    .pipe(swig(options))
+    .pipe(gulp.dest('./src/parts'));
+});
+
+gulp.task('html-pages', function(){
+    var options = { cache:false, load_json:true, varControls: ['{{@page', '}}'] };
+    return gulp.src(['./src/pages/**/*.jst'])
+    .pipe(swig(options))
+    .pipe(gulp.dest('./src/pages'));
+});
+
+gulp.task('html', ['html-pieces', 'html-parts', 'html-pages'], function() {
+   return this; 
+});
+
+gulp.task('clean', function(){
+    return gulp.src(['./src/**/*.html', './build/*.html', './build/*.js', './build/*.css'], { read: false })
+    .pipe(rimraf({ force: true }));
 });
